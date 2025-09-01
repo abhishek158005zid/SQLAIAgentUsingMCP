90e469aa73fe9b9e78f51bbd987ed58713664508
abhishek.jain@zid.sa <abhishek158005zid>
2025-09-01 12:16:16 +0530

    update settings.py

---
+++ settings.py
@@ -0,0 +1,12 @@
+from typing import Optional, Literal
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+class ServerConfig:
+    db_url: str = os.getenv("DB_URL")
+    db_user: str = os.getenv("DB_USER")
+    db_password: str = os.getenv("DB_PASSWORD")
+    mcp_transport: Literal["stdio", "streamable-http"] = "streamable-http"
+    schema_file:str = "_api__analytics__models.yml"
